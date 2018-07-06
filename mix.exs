f583763c0c70decd1a9763e8bac761e383af112d
Jake Morrison <jake@cogini.com>
2018-07-06 13:14:05 +0800

    Update library dependencies

--- mix.exs
+++ mix.exs
@@ -33,9 +33,10 @@ defmodule ExDash.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:ex_doc, "~> 0.16"},
-      {:floki, "~> 0.14.0"},
-      {:cortex, "~> 0.1"}
+      {:ex_doc, "~> 0.18.3"},
+      {:floki, "~> 0.20.3"},
+      {:mochiweb_html, "~> 2.15"},
+      {:cortex, "~> 0.5.0"}
     ]
   end
 
